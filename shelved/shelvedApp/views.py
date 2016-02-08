fe3bfdae4cfb93b2a7a635dc502b054faac3aa13
Shuixi Li <shuixili@gmail.com>
2016-02-08 15:50:28 -0500

    recreated the project with django1.5

--- shelved/shelvedApp/views.py
+++ shelved/shelvedApp/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
-
 # Create your views here.
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+
+def play(request):
+    if request.method == 'GET':
+        print('user clicked yes ');
+
+        return HttpResponse("hello world",status=200)
