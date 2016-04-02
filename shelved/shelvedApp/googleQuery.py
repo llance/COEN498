59e3f894a8b5b1a9a9b31a12a126c1e34796fcdf
Vincent <old.headlines@gmail.com>
2016-04-02 16:36:15 -0400

    Initial Google Query code

---
+++ shelved/shelvedApp/googleQuery.py
@@ -0,0 +1,27 @@
+__author__ = 'vbilodeau'
+
+import datetime
+import requests
+from django.utils import timezone
+import json
+from apiclient.discovery import build
+
+#Set up app key
+books_service = build('books', 'v1', developerKey='AIzaSyD6P55381pkncIFbOvxP-Ov0sYt-lcoOP8')
+
+# gets a UPC OR ISBN
+def queryGoogle(isbn):
+    query = str(isbn)
+    request = books_service.volumes().list(source='public', q=query)
+    books = request.execute()
+    if (books['items'][0]):
+        book = books[‘items’][0]
+        title = (book["volumeInfo"]["title"])
+        #find a faster streamlined less retarded way
+        if (book["volumeInfo"]["subtitle"]):
+            subtitle = (book["volumeInfo"]["subtitle"])
+        authors = (book["volumeInfo"]["authors"])
+        pageCount = (book["volumeInfo"]["pageCount"])
+        publisher = (book["volumeInfo"]["publisher"])
+        publishedDate = (book["volumeInfo"]["publishedDate"])
+        language = (book["volumeInfo"]["language"])
