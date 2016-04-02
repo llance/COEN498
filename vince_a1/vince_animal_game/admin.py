6b9248e52b50719b2bb9493c322497f08f20401f
Vincent <old.headlines@gmail.com>
2016-04-02 15:28:04 -0400

    Cleanup, removed vince_a1

--- vince_a1/vince_animal_game/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import Country
-from .models import Question
-
-admin.site.register(Country)
-admin.site.register(Question)
--- vince_a1/vince_animal_game/apps.py
+++
@@ -1,7 +0,0 @@
-from __future__ import unicode_literals
-
-from django.apps import AppConfig
-
-
-class VinceAnimalGameConfig(AppConfig):
-    name = 'vince_animal_game'
