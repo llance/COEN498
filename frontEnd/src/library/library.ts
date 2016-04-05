9503b6a45c84e7014d6b9eaed7141400b60d517b
Shuixi Li <shuixili@gmail.com>
2016-04-05 15:01:25 -0400

    attempt to display db data in a table

--- frontEnd/src/library/library.ts
+++ frontEnd/src/library/library.ts
@@ -21,6 +21,39 @@ let template = require('./library.html');
     styles: [styles]
 })
 export class Library {
+    // var mockDataForThisTest = "json=" + encodeURI(JSON.stringify([
+    //     {
+    //         id: 1,
+    //         firstName: "Peter",
+    //         lastName: "Jhons"
+    //     },
+    //     {
+    //         id: 2,
+    //         firstName: "David",
+    //         lastName: "Bowie"
+    //     }
+    // ]));
+
+
+    // var app = angular.module('myApp', []);
+
+    // function PeopleCtrl($scope, $http) {
+
+    //     $scope.people = [];
+
+    //     $scope.loadPeople = function() {
+    //         var httpRequest = $http({
+    //             method: 'POST',
+    //             url: '/echo/json/',
+    //             data: mockDataForThisTest
+
+    //         }).success(function(data, status) {
+    //             $scope.people = data;
+    //         });
+
+    //     };
+
+    // }
 
     constructor(public router: Router, public http: Http) {
     }
